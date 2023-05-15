dc4d6389c2f792608591146f52640860ea2d5d84
Jeff Davis <jeffda@vmware.com>
2023-05-15 19:09:12 -0400

    feat: add gcp kms backed sops
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

---
+++ provision/gcp-kms.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.5.0"
+    }
+  }
+}
+
+provider "google" {}
+
+resource "google_project_service" "kms_api" {
+  ## Enabling GCP KMS API
+  project            = var.project
+  service            = "cloudkms.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_kms_key_ring" "my_key_ring" {
+  # Create the key ring on GCP
+  project  = var.project
+  name     = "davishaus"
+  location = "global"
+  depends_on = [
+  google_project_service.kms_api]
+}
+
+resource "google_kms_crypto_key" "my_crypto_key" {
+  # Add a new create to the key ring
+  name     = "sops"
+  key_ring = google_kms_key_ring.my_key_ring.id
+}
+
+variable "project" {
+  description = "The GCP project"
+  type        = string
+  default     = "kms-vault-363503"
+}
