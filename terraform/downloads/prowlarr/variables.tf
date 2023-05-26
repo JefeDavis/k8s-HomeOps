74ee23f88731d04a32d7a59198c25a873ac8155a
Jeff Davis <jeffda@vmware.com>
2023-05-26 18:54:36 -0400

    feat(prowlarr): add terraform
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

---
+++ terraform/downloads/prowlarr/variables.tf
@@ -0,0 +1,49 @@
+variable "PROWLARR_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "LIDARR_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "READARR_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "RADARR_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "SONARR_API_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "prowlarr_url" {
+  type    = string
+  default = "http://prowlarr.downloads.svc.cluster.local:9696"
+}
+
+variable "lidarr_url" {
+  type    = string
+  default = "http://lidarr.media.svc.cluster.local:8686"
+}
+
+variable "readarr_url" {
+  type    = string
+  default = "http://readarr.media.svc.cluster.local:8787"
+}
+
+variable "radarr_url" {
+  type    = string
+  default = "http://radarr.media.svc.cluster.local:7878"
+}
+
+variable "sonarr_url" {
+  type    = string
+  default = "http://sonarr.media.svc.cluster.local:8989"
+}
