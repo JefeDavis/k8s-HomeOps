625f3d5c1075ca61587abe26c596f77e60d67ebb
Jeff Davis <jeffda@vmware.com>
2023-06-03 02:10:37 -0400

    feat(authentik): add users and groups
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- terraform/security/authentik/groups.tf
+++ terraform/security/authentik/groups.tf
@@ -2,21 +2,9 @@ data "authentik_group" "authentik-admins" {
   name = "authentik Admins"
 }
 
-resource "authentik_group" "davishaus-admins" {
-  name         = "DavusHaus Admins"
-  parent       = data.authentik_group.authentik-admins.id
-  is_superuser = true
-  # users        = ""
-}
-
-resource "authentik_group" "grafana-admins" {
-  name         = "Grafana Admins"
-  parent       = authentik_group.davishaus-admins.id
-  is_superuser = false
-}
-
-resource "authentik_group" "sonarr-admins" {
-  name         = "Sonarr Admins"
-  parent       = authentik_group.davishaus-admins.id
-  is_superuser = false
+resource "authentik_group" "groups" {
+  for_each = var.groups
+  name     = each.value.name
+  # parent       = authentik_group.groups[each.value.parent].id
+  is_superuser = each.value.superuser
 }
