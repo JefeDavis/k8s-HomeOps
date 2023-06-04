a096de34b81dc9d63647014d1babec37e1d66fd8
Jeff Davis <jeffda@vmware.com>
2023-06-04 02:10:46 -0400

    feat(authentik): add plex allowed servers
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- terraform/security/authentik/flows.tf
+++ terraform/security/authentik/flows.tf
@@ -2,6 +2,10 @@ data "authentik_flow" "default-authorization-flow" {
   slug = "default-provider-authorization-implicit-consent"
 }
 
+data "authentik_flow" "default-source-authentication" {
+  slug = "default-source-authentication"
+}
+
 data "authentik_flow" "default-authentication-flow" {
   slug = "default-authentication-flow"
 }
