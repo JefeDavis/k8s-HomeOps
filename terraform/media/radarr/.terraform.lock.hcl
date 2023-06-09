5ca39610a0c4ebe54be9b718e1988ed332be7155
Jeff Davis <jeffda@vmware.com>
2023-06-08 23:43:26 -0400

    feat(radarr): add terraform
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

---
+++ terraform/media/radarr/.terraform.lock.hcl
@@ -0,0 +1,25 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/devopsarr/radarr" {
+  version     = "1.8.0"
+  constraints = "1.8.0"
+  hashes = [
+    "h1:pbR/lshzqwAoueVTE3Ta9S6PG6ZcS2TrO90T9u5cUUU=",
+    "zh:08b94b580b0962d798cf6ba89c449799d95eb7392b706a8a4408539d6821a894",
+    "zh:135e3f2deb382bfa104ea8df300bca9fe08c9fd1be3c7950cd37bffd308ee52d",
+    "zh:16e69cde9c1658410a4654c97f3096c36df332c9dc0b77243431b736991bd011",
+    "zh:5ce595a967a4768241cb66dfb864068ae389196dc1d595aaca07ee5550a04b94",
+    "zh:5ead7bc1172bc338a95778021906fc3466b997813c578ce6f9cc4d428f312e23",
+    "zh:6a2c7422836fde2cc651cc1cf973d050388dc3692bae0b229acc53d0a45423f0",
+    "zh:6eb401e3dc9102b54d776ef55970a256b3e0488e48ea2c25cfc1b6fe33c2cce4",
+    "zh:6f3581ff827ccbf07a38fb076ba3942df1ed5325f00f0bd806297464ae0839df",
+    "zh:7e8698baae4e8c4c9fab76143e7617df4e2c3cfbd8befc26469662271225a1e5",
+    "zh:8599207c3ccc1be8d8cf17d4d3dc7d520826019027abf0f113390d2de666a2bd",
+    "zh:890df766e9b839623b1f0437355032a3c006226a6c200cd911e15ee1a9014e9f",
+    "zh:bf9c76ba10ceb4c519fbb080c6acb95658eddcfd70d8973c9b0d03d0779ade66",
+    "zh:e52a3e5fd6855de575f1198b4b596a09e9d51ca29bb481ecb7d46befcaa6bb6b",
+    "zh:e663f784b662f5e91db8af7629b4f86e5e6fa92b9ab5fe9982ba9802d70654a1",
+    "zh:f82cc337acc4c0fac62678f8399e47a147964206a1112a94bf8e7aa4b667a016",
+  ]
+}
---
+++ terraform/media/radarr/media-management.tf
@@ -0,0 +1,35 @@
+resource "radarr_media_management" "settings" {
+  auto_unmonitor_previously_downloaded_movies = false
+  copy_using_hardlinks                        = true
+  create_empty_movie_folders                  = false
+  delete_empty_folders                        = false
+  auto_rename_folders                         = true
+  download_propers_and_repacks                = "doNotPrefer"
+  skip_free_space_check_when_importing        = false
+  minimum_free_space_when_importing           = 100
+  set_permissions_linux                       = false
+  chmod_folder                                = "755"
+  chown_group                                 = "kah"
+  paths_default_static                        = false
+  enable_media_info                           = true
+  import_extra_files                          = true
+  extra_file_extensions                       = "srt"
+  file_date                                   = "none"
+  recycle_bin_cleanup_days                    = 7
+  recycle_bin                                 = "/media/library/trash"
+  rescan_after_refresh                        = "always"
+}
+
+resource "radarr_naming" "naming" {
+  include_quality            = false
+  rename_movies              = true
+  replace_illegal_characters = false
+  replace_spaces             = false
+  colon_replacement_format   = "dash"
+  standard_movie_format      = "{Movie CleanTitle} {(Release Year)} {imdb-{ImdbId}} {edition-{Edition Tags}} {[Custom Formats]}{[Quality Full]}{[MediaInfo 3D]}{[MediaInfo VideoDynamicRangeType]}{[Mediainfo AudioCodec}{ Mediainfo AudioChannels}]{MediaInfo AudioLanguages}[{MediaInfo VideoBitDepth}bit][{Mediainfo VideoCodec}]{-Release Group}"
+  movie_folder_format        = "{Movie CleanTitle} ({Release Year})"
+}
+
+resource "radarr_root_folder" "series" {
+  path = "/media/library/movies"
+}
