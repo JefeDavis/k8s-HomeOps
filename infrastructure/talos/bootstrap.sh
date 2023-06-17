4db5c7a65bbe8e847076b91a1dd0c303b4006374
Jeff Davis <jeffda@vmware.com>
2023-06-16 21:23:28 -0400

    feat(talos): add 3rd nuc
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- infrastructure/talos/bootstrap.sh
+++ infrastructure/talos/bootstrap.sh
@@ -4,7 +4,7 @@ echo "Applying Node Configs"
 # Deploy the configuration to the nodes
 talosctl apply-config --insecure -n 10.77.0.11 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-001.internal.davishaus.dev.yaml
 talosctl apply-config --insecure -n 10.77.0.12 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-002.internal.davishaus.dev.yaml
-# talosctl apply-config --insecure -n 10.77.0.13 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-003.internal.davishaus.dev.yaml
+talosctl apply-config --insecure -n 10.77.0.13 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-003.internal.davishaus.dev.yaml
 
 echo "Sleeping..."
 sleep 120
