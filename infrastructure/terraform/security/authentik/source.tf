e61444be55daa1522d70d1a608229431601b6c26
Jeff Davis <mr.jefedavis@gmail.com>
2024-04-27 19:35:30 -0400

    fix: quoting

--- infrastructure/terraform/security/authentik/source.tf
+++ infrastructure/terraform/security/authentik/source.tf
@@ -5,7 +5,7 @@ resource "authentik_source_plex" "plex" {
   plex_token          = var.AUTHENTIK_PLEX_TOKEN
   authentication_flow = data.authentik_flow.default-source-authentication.id
   enrollment_flow     = data.authentik_flow.default-enrollment-flow.id
-  user_matching_mode  = email_link
+  user_matching_mode  = "email_link"
   allow_friends       = false
   allowed_servers = [
     var.PLEX_SERVER_ID
