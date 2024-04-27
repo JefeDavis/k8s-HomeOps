f83da7d22191ccafcec0cb7155d0513fb2f1d6e2
Jeff Davis <mr.jefedavis@gmail.com>
2024-04-27 19:39:53 -0400

    fix: set brand default status

--- infrastructure/terraform/security/authentik/brands.tf
+++ infrastructure/terraform/security/authentik/brands.tf
@@ -4,7 +4,7 @@ data "authentik_brand" "authentik-default" {
 
 resource "authentik_brand" "davishaus" {
   domain              = var.external_domain
-  default             = true
+  default             = false
   branding_title      = "Davishaus"
   flow_authentication = authentik_flow.davishaus-authentication.uuid
   flow_invalidation   = data.authentik_flow.default-invalidation-flow.id
