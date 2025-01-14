71875103a13766994b525057703b23bb9a2a7c12
Jeff Davis <mr.jefedavis@gmail.com>
2025-01-13 23:33:29 -0500

    refactor: use flux-operator and task files

--- kubernetes/jsdyb-k8s-001/talos/apply.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -n 10.77.0.11 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-001.internal.davishaus.dev.yaml
-talosctl apply-config -n 10.77.0.12 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-002.internal.davishaus.dev.yaml
-talosctl apply-config -n 10.77.0.13 -f ./clusterconfig/jsdyb-k8s-001-jsdyb-nuc-003.internal.davishaus.dev.yaml
